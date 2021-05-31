99e180c5e6bffd9082886ef6ec69e754267bab2c
Alex Fedorchenko <mralex.fed@gmail.com>
2021-05-31 18:39:56 +0300

    Updates

---
+++ precache-manifest.2dd1a63921a6becacb8299acf3f6c5d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a9249c6f4ae1db595e778bb7e8a18a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8ecc25d84e11476516bb",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "b1ac01fc1f3a48e97cf4",
+    "url": "./static/js/2.08db3e67.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.08db3e67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ecc25d84e11476516bb",
+    "url": "./static/js/main.34adf40e.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
